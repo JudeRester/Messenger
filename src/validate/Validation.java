f690760f84c2b5e27f01a3b989412c7beebde960
JudeRester <llkkeor@naver.com>
2018-04-06 04:58:36 +0900

    z

--- src/validate/Validation.java
+++ src/validate/Validation.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Validation {
 	public boolean EmailVali(String email) {
-		String pattern = "^[a-zA-Z0-9\\-_]+@[a-zA-Z0-9\\-_\\.]+\\.[a-zA-Z]+$";
+		String pattern = "^[a-zA-Z0-9\\-_\\.]+@[a-zA-Z0-9\\-_\\.]+\\.[a-zA-Z]+$";
 		boolean a = Pattern.matches(pattern, email);				
 		
 		return a;
